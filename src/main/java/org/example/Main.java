8d0277125d4ca9e62d2cb4620a35dea1660ef6e3
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2024-12-26 17:22:53 +0100

    Client Streamig

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+    public void main(String[] args) {
+        Server server = ServerBuilder.forPort(8080)
+                .addService(new MessageService())
+                .build();
+        try {
+            server.start();
+            System.out.println("Server started at " + server.getPort());
+            server.awaitTermination();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }}
--- src/main/java/org/example/MessageService.java
+++ src/main/java/org/example/MessageService.java
@@ -1,4 +1,42 @@
 package org.example;
 
-public class MessageService {
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+public class MessageService extends SalutationGrpc.SalutationImplBase {
+    @Override
+    public StreamObserver<SalutationOuterClass.SalutRequest> message(StreamObserver<SalutationOuterClass.SalutResponse> responseObserver) {
+        return new StreamObserver<>() {
+            StringBuilder names = new StringBuilder();
+
+            @Override
+            public void onNext(SalutationOuterClass.SalutRequest salutRequest) {
+                names.append(salutRequest.getName()).append(", ");
+
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.err.println("Erreur reçue : " + t.getMessage());
+            }
+            @Override
+            public void onCompleted() {
+// Envoie une réponse au client une fois que tous les messages sont reçus
+                String responseMessage = "Salut à tous : " + names.toString();
+                SalutationOuterClass.SalutResponse response = SalutationOuterClass.SalutResponse.newBuilder()
+                        .setMessage(responseMessage)
+                        .build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+
 }
+
+
